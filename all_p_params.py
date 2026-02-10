a8f66be8ed67cdc82a11cadaa351951875c97afb
hassanalam014 <hassanalam014@gmail.com>
2026-02-10 22:50:11 +0000

    improved tait fit

--- all_p_params.py
+++ all_p_params.py
@@ -5,7 +5,6 @@
 #======================================================
 # Data 1 Very Good Matching 1
 # Paper: Good 2000 PVT of PMMA Schmidt ma991722h
-# Author: Schmidt et al. (2000)
 
 Pstar_1=577.933210    #+/- 22.7932121 (3.94%)
 Tstar_1=677.583131    #+/- 7.01760682 (1.04%)
@@ -16,7 +15,6 @@ Rstar_1=1.27392163    #+/- 0.00421348 (0.33%)
 #======================================================
 # Data 2 Very Good Matching 2
 # Paper: Best 2011 PVT and Tg of PMMA Grassia 1-s2.0-S0022309310005338-main
-# Author: Grassia et al. (2011)
 
 Pstar_2=562.397384    #+/- 7.52128331 (1.34%)
 Tstar_2=654.343418    #+/- 2.72548523 (0.42%)
@@ -27,7 +25,6 @@ Rstar_2=1.28146781    #+/- 0.00153252 (0.12%)
 #======================================================
 # Data 3 Overlap and Significant Matching but Not Complete Matching
 # Paper: Old 1992 PVT of PMMA PII_ 0032-3861(92)90694-R
-# Author: Walsh et al. (1992)
 
 Pstar_3=564.823646    #+/- 17.9901923 (3.19%)
 Tstar_3=667.892308    #+/- 5.07983058 (0.76%)
@@ -38,7 +35,6 @@ Rstar_3=1.28420005    #+/- 0.00323659 (0.25%)
 #======================================================
 # Data 4 Neither Matching Nor Overlap
 # Paper: Good 2001 PVT and Tg of PMMA Wen ma010023d
-# Author: Wen (2001)
 
 Pstar_4=479.377365    #+/- 31.3221097 (6.53%)
 Tstar_4=709.595728    #+/- 7.99245188 (1.13%)
